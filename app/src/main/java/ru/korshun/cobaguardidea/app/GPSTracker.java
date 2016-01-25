0cd94d573687f1a8f7575d448e7660093d238be6
korshunRU <kanibal_2002@mail.ru>
2016-01-25 20:00:23 +0500

    1

---
+++ app/src/main/java/ru/korshun/cobaguardidea/app/GPSTracker.java
@@ -0,0 +1,183 @@
+package ru.korshun.cobaguardidea.app;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class GPSTracker
+        implements LocationListener {
+
+
+    private Context mContext;
+
+    // Flag for GPS status
+    private boolean isGPSEnabled = false;
+
+    // Flag for network status
+    private boolean isNetworkEnabled = false;
+
+    // Flag for GPS status
+    private boolean canGetLocation = false;
+
+    private Location location; // Location
+    private double latitude; // Latitude
+    private double longitude; // Longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute
+
+    // Declaring a Location Manager
+    private LocationManager locationManager;
+
+
+    public GPSTracker(Context context) {
+
+        this.mContext = context;
+
+        getLocation();
+
+    }
+
+
+    private Location getLocation() {
+        try {
+            locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+
+            // Getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // Getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // No network provider is enabled
+            }
+            else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+
+                    locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // If GPS enabled, get latitude/longitude using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+
+    /**
+     * Function to get latitude
+     * */
+    public double getLatitude(){
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+
+        // return latitude
+        return latitude;
+    }
+
+
+    /**
+     * Function to get longitude
+     * */
+    public double getLongitude(){
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+
+        // return longitude
+        return longitude;
+    }
+
+
+
+    public Barcode.GeoPoint getLocationFromAddress(String strAddress) {
+
+        Geocoder coder = new Geocoder(mContext);
+        List<Address> address;
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Address location = address.get(0);
+        location.getLatitude();
+        location.getLongitude();
+
+        return new Barcode.GeoPoint(1, (int) (location.getLatitude() * 1E6), (int) (location.getLongitude() * 1E6));
+    }
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        System.out.println("myLog: onLocationChanged " + location.getProvider() + " " + location.getLatitude() + " " + location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
